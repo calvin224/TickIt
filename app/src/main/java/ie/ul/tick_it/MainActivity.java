6955f5c8a64fd6634089e1dc480aac4db675c54d
calvin224 <calvinpower44@gmail.com>
2022-02-25 00:45:05 +0000

    Log-in, log-out,register and profile loading now working

--- app/src/main/java/ie/ul/tick_it/MainActivity.java
+++ app/src/main/java/ie/ul/tick_it/MainActivity.java
@@ -1,15 +1,26 @@
 package ie.ul.tick_it;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private TextView register;
+    private EditText Email, Password;
+    private Button signIn;
 
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +29,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         register = (TextView) findViewById(R.id.Register);
         register.setOnClickListener(this);
-    }
 
+        signIn = (Button) findViewById(R.id.signIn);
+        signIn.setOnClickListener(this);
+
+        Email = (EditText) findViewById(R.id.editTextEmail);
+        Password = (EditText) findViewById(R.id.editTextPassword);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.Register:
             startActivity(new Intent(this,RegisterUser.class));
             break;
+
+            case R.id.signIn:
+                userLogin();
         }
     }
+    private void userLogin() {
+        String email = Email.getText().toString().trim();
+        String password = Password.getText().toString().trim();
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    startActivity(new Intent(MainActivity.this,ProfileActivity.class));
+                }
+            }
+        });
+    }
+
+
+
 }
--- app/src/main/java/ie/ul/tick_it/RegisterUser.java
+++ app/src/main/java/ie/ul/tick_it/RegisterUser.java
@@ -1,9 +1,12 @@
 package ie.ul.tick_it;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,19 +15,26 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.w3c.dom.Text;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterUser extends AppCompatActivity implements View.OnClickListener {
     private FirebaseAuth mAuth;
     private TextView banner, RegisterUser;
     private EditText Name, Age, EmailAddress, Password;
     private ProgressBar progressBar;
-
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +42,6 @@ public class RegisterUser extends AppCompatActivity implements View.OnClickListe
         mAuth = FirebaseAuth.getInstance();
         RegisterUser = (Button) findViewById(R.id.RegisterUser);
         RegisterUser.setOnClickListener(this);
-
         Name = (EditText) findViewById(R.id.Name);
         Age = (EditText) findViewById(R.id.Age);
         EmailAddress = (EditText) findViewById(R.id.EmailAddress);
@@ -48,20 +57,35 @@ public class RegisterUser extends AppCompatActivity implements View.OnClickListe
                 break;
         }
     }
-
     private void RegisterUser() {
         String name = Name.getText().toString().trim();
         String age = Age.getText().toString().trim();
         String Email = EmailAddress.getText().toString().trim();
         String password = Password.getText().toString().trim();
-
         mAuth.createUserWithEmailAndPassword(Email, password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
+                            Map<String, Object> userMap = new HashMap<>();
+                            userMap.put("Name", name);
+                            userMap.put("Email", Email);
+                            userMap.put("Age", age);
+                            db.collection("Users").document(FirebaseAuth.getInstance().getUid())
+                                    .set(userMap)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            Log.d(TAG, "DocumentSnapshot successfully written!");
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.w(TAG, "Error writing document", e);
+                                        }
+                                    });
                             User user = new User(name, age, Email);
-
                             FirebaseDatabase.getInstance().getReference("User")
                                     .child(FirebaseAuth.getInstance().getUid())
                                     .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
@@ -77,8 +101,6 @@ public class RegisterUser extends AppCompatActivity implements View.OnClickListe
                             });
                         }
                     }
-
                 });
-
     }
 }
